5043e91e0f0e1f191443a7aad01ea4f008a2878b
mttble <matthew.beale1@hotmail.com>
2023-06-22 11:51:01 +0930

    added vaccination and licence models. added vaccinations and licences in seed function. Automated status  of courses, licences and vaccinations

--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -3,6 +3,8 @@ from init import db, bcrypt
 from datetime import date
 from models.user import User
 from models.course import Course
+from models.licence import Licence
+from models.vaccination import Vaccination
 
 cli_bp = Blueprint('db', __name__)
 
@@ -30,23 +32,119 @@ def seed_db():
     db.session.add_all(users)
     db.session.commit()
 
-    courses = [
+    courses_data = [
         Course(
             title="LV Rescue and Resuscitation",
-            status="In Date",
+            status="In date" if date(2024, 6, 21) >= date.today() else "Out of date",
             date_of_completion=date(2023, 6, 21),
             date_of_expiry=date(2024, 6, 21)
         ),
         Course(
             title="High Voltage Terminations",
-            status="In Date",
+            status="In date" if date(2024, 5, 22) >= date.today() else "Out of date",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2022, 5, 22)
+        )
+    ]
+    
+    # Auto fill for status In date or Out of date depending on date of expiry
+    courses = []
+    for course in courses_data:
+        course.status = "In date" if course.date_of_expiry >= date.today() else "Out of date"
+        courses.append(course)
+
+    db.session.query(Course).delete()
+    db.session.add_all(courses)
+    db.session.commit()
+
+    licences_data = [
+        Licence(
+            title="Electrical",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 6, 21),
+            date_of_expiry=date(2024, 6, 21)
+        ),
+        Licence(
+            title="White Card",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2024, 5, 22)
+        ),
+        Licence(
+            title="Yellow Card",
+            number="",
+            description="",
+            status="",
             date_of_completion=date(2023, 5, 22),
             date_of_expiry=date(2024, 5, 22)
         )
     ]
 
-    db.session.query(Course).delete()
-    db.session.add_all(courses)
+    # Auto fill for status In date or Out of date depending on date of expiry
+    licences = []
+    for licence in licences_data:
+        licence.status = "In date" if licence.date_of_expiry >= date.today() else "Out of date"
+        licences.append(licence)
+
+    db.session.query(Licence).delete()
+    db.session.add_all(licences)
+    db.session.commit()
+
+    vaccinations_data = [
+        Vaccination(
+            title="Influenza",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 6, 21),
+            date_of_expiry=date(2024, 6, 21)
+        ),
+        Vaccination(
+            title="Covid",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2024, 5, 22)
+        ),
+        Vaccination(
+            title="Tetanus",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2024, 5, 22)
+        ),
+        Vaccination(
+            title="Hepatitis A",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2024, 5, 22)
+        ),
+        Vaccination(
+            title="Hepatitis B",
+            number="",
+            description="",
+            status="",
+            date_of_completion=date(2023, 5, 22),
+            date_of_expiry=date(2024, 5, 22)
+        )
+    ]
+
+    # Auto fill for status In date or Out of date depending on date of expiry
+    vaccinations = []
+    for vaccination in vaccinations_data:
+        vaccination.status = "In date" if vaccination.date_of_expiry >= date.today() else "Out of date"
+        vaccinations.append(vaccination)
+
+    db.session.query(Vaccination).delete()
+    db.session.add_all(vaccinations)
     db.session.commit()
 
     print("Models seeded")
---
+++ models/vaccination.py
@@ -0,0 +1,13 @@
+from init import db
+from datetime import date
+
+class Vaccination(db.Model):
+    __tablename__ = 'vaccinations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    number = db.Column(db.String(30))
+    description = db.Column(db.String(100))
+    status = db.Column(db.String(30))
+    date_of_completion = db.Column(db.Date)
+    date_of_expiry = db.Column(db.Date)
